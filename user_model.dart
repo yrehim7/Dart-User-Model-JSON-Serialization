1a10d5fbedac07b09bcff34ad72c194248d3dd07
yrehim7 <54071798+yrehim7@users.noreply.github.com>
2022-12-08 11:04:56 +0100

    Update user_model.dart

--- user_model.dart
+++ user_model.dart
@@ -7,13 +7,11 @@ class UserModel {
     UserModel userFromJson(String user) =>
         UserModel.fromJson(json.decode(user));
 
-  //* outgoing data to Objects
     factory UserModel.userFromJson(Map<String, dynamic> json) =>
         UserModel(
             user: userFromJson(json['user']),
             token: json['token'],
         ); //Usermodel
-  //*  outgoing to JSON *//
     Map<String, dynamic> fromObject() => {
         "user" : user.fromObject(),
         "token" : token,
@@ -28,14 +26,12 @@ class User {
     String dateofbirth;
     
     User({required this.name, required this.usergroup, required this.email, required this.dateofbirth});
- 
-  //* outgoing data to Objects
+
     factory User.fromJson(Map<String, dynamic> json) => 
         User(name: json['name'], usergroup: json['usergroup'] email: json['email'], dateofbirth: json['dateofbirth'], phone: ['phone']);
-  
-  //*  outgoing to JSON
+
     Map<String, dynamic> fromObject() => {
-        "name" : name,
+        "name" : na`    `11me,
         "email" : email,
         "usergroup" : usergroup,
         "dateofbirth" : dateofbirth,
